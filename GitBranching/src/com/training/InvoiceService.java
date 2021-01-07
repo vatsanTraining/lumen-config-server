0bcdbb895586aedf6f80429f27afa3b5e9dbb406
vatsank <vatsank@gmail.com>
2021-01-07 19:07:04 +0530

    Merged

--- GitBranching/src/com/training/InvoiceService.java
+++ GitBranching/src/com/training/InvoiceService.java
@@ -2,15 +2,15 @@ package com.training;
 
 public class InvoiceService {
 
-<<<<<<< HEAD
+
 	
 	public int addInvoice(Invoice entity) {
 		
 		 return 0;
-=======
+	}
+	
 	public Invoice show() {
 		
 		return new Invoice();
->>>>>>> refs/heads/master
 	}
 }
