ce20825cb009e88fb4d98d9fe30b71c90bf536e3
vatsank <vatsank@gmail.com>
2021-01-07 18:40:32 +0530

    Added Equals and HashCode Method

--- GitBranching/src/com/training/Invoice.java
+++ GitBranching/src/com/training/Invoice.java
@@ -25,6 +25,34 @@ public class Invoice {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Invoice other = (Invoice) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 	@Override
 	public String toString() {
 		return "Invoice [id=" + id + ", name=" + name + "]";
